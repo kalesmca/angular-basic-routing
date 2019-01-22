7348cc06d179ad352d025888c2fddee34430f50d
Kaleeswaran <kaleeswaran@iinerds.com>
2019-01-22 22:11:26 +0530

    pushing array issue need to fix

--- student-mark-statement-app/src/app/app-routing.module.ts
+++ student-mark-statement-app/src/app/app-routing.module.ts
@@ -4,14 +4,16 @@ import { CreateStudent } from '../app/createPage/create.component';
 import { ShowAll } from '../app/listPage/list.component';
 import { from } from 'rxjs';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 
 const routes: Routes = [{ path: 'create', component: CreateStudent },
-{ path: 'show-all', component: ShowAll }
+{ path: 'show-all', component: ShowAll },
+{ path: '', redirectTo: '/create', pathMatch: 'full' }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes), FormsModule],
+  imports: [RouterModule.forRoot(routes), FormsModule, CommonModule],
   exports: [RouterModule],
   declarations: [CreateStudent, ShowAll]
 })
--- student-mark-statement-app/src/app/app.module.ts
+++ student-mark-statement-app/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProfileService } from '../app/common/services/profile.service';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,7 @@ import { AppComponent } from './app.component';
     AppRoutingModule,
     FormsModule
   ],
-  providers: [],
+  providers: [ProfileService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
